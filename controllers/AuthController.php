43193e4b4c1afc7a50123405e0e8e0cb94b1cb1d
Chrisando Ryan <chrisando.siahaan@gmail.com>
2023-09-26 15:10:22 +0700

    S3: basic session and cookie

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,5 +1,5 @@
 <?php
-    $is_login = false;
+    session_start();
 
     if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
@@ -8,9 +8,16 @@
 
         if ($username === "admin" && $password === "admin") {
             echo "Login Success";
+            
+            $_SESSION['is_login'] = true;
+            $_SESSION['username'] = $username;
+
+            header("Location: ../messages.php");
         }
         else {
             echo "Login Failed";
+
+            header("Location: ../login.php");
         }
 
         
--- messages.php
+++ messages.php
@@ -1,8 +1,10 @@
 <?php
     session_start();
-    if ($_SESSION['login'] !== true) {
+
+    if($_SESSION['is_login'] !== true) {
         header("Location: login.php");
     }
+
 ?>
 
 <!DOCTYPE html>
