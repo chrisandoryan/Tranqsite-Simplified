38ab350b60b517c9daa47bade7f1326301b29fbd
Chrisando Ryan <chrisando.siahaan@gmail.com>
2023-09-26 14:51:55 +0700

    S2: basic login validation

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,55 +1,20 @@
 <?php
-    session_start();
     $is_login = false;
-
-    var_dump($is_login == 0);
-    var_dump($is_login === 0);
-
-    $usernames = [
-      "admin",
-      "dalgona",
-      "parg29",
-      "matchalover",
-      "aseng",
-      "subwayodading"
-    ];
-
-    $passwords = [
-        "admin",
-        "tidakbikinkembung",
-        "supershy",
-        "greentea",
-        "sepuh",
-        "$ugwey"
-    ];
 
     if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        // var_dump($username);
-        // var_dump($password);
-
-        for ($i = 0; $i < count($usernames); $i++) {
-            if ($username === $usernames[$i] && $password === $passwords[$i]) {
-                $is_login = true;
-                break;
-            }
-        }
-
-        if ($is_login) {
-            $_SESSION["success_message"] = "Login Success";
-
-            $_SESSION['login'] = true;
-            $_SESSION['username'] = $username;
-
-            header("Location: ../messages.php");
+        if ($username === "admin" && $password === "admin") {
+            echo "Login Success";
         }
         else {
-            $_SESSION["error_message"] = "Login Failed";
-
-            header("Location: ../login.php");
+            echo "Login Failed";
         }
+
+        
+
+        
 
     }
