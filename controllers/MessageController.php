98f54bd0b576c259ffd2c95ddee30041a40bfa71
Chrisando Ryan <chrisando.siahaan@gmail.com>
2023-11-21 15:07:11 +0700

    P8: web form security

--- controllers/MessageController.php
+++ controllers/MessageController.php
@@ -5,11 +5,49 @@
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
 
+    function checkInputLength($string, $max_length) {
+        if (empty($string) || $string == "" || strlen($string) > $max_length) {
+            return false;
+        }
+        return true;
+    }
+
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $title = $_POST['title'];
         $recipient = $_POST['recipient'];
         $message = $_POST['message'];
         $sender_id = $_SESSION['id'];
+
+        // TODO: sanitize and validate $title input 
+        // Assigned to: Group 1
+        // title must not be empty
+        // title < 20 chars
+        // title must be robust against xss and html injection. any html tags must be stripped.
+
+        // TODO: validate and sanitize $message input
+        // Assigned to: Group 2
+
+        // message < 100
+        // message cannot be empty
+        // message > 5 words
+        // message must be robust against xss and html injection. any html tags must be escaped.
+
+        // TODO: sanitize and validate $recipient input
+        // Assigned to: Group 3
+
+        // recipient must not be empty
+        // recipient must be between 1 - 4 (inclusive)
+        // recipient must be a digit
+
+        // TODO: validate and sanitize file input
+        // Assigned to: Group 4 & Group 5
+        
+        // file extension must be: .pdf, .jpeg, .docx, .txt, .xlsx, .csv, .png, .mp3, .mp4, .pptx, .mkv
+        // file size <= 25MB
+        // file size > 0
+        // file name length < 50
+        // file name must not contain path element (./, ../, etc.)
+        
 
         $attachment = $_FILES['user_file'];
 
